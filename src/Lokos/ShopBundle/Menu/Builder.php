5b8e5786161557290e09ceac179972dfbc432908
stsybulniak <stsybulniak@mindk.com>
2017-03-30 19:22:26 +0300

    tmp

--- src/Lokos/ShopBundle/Menu/Builder.php
+++ src/Lokos/ShopBundle/Menu/Builder.php
@@ -5,6 +5,7 @@ namespace Lokos\ShopBundle\Menu;
 use Doctrine\ORM\EntityManager;
 use Knp\Menu\FactoryInterface;
 use Lokos\ShopBundle\Entity\Category;
+use Lokos\ShopBundle\Repositories\CategoryRepository;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
@@ -34,31 +35,84 @@ class Builder implements ContainerAwareInterface
         );
 
 
+                /** @var CategoryRepository $repo */
+                $repo = $this->container->get('doctrine')->getManager()->getRepository('LokosShopBundle:Category');
 
+                /** @var EntityManager $em */
+        $em = $this->container->get('doctrine.orm.entity_manager');
+//        $phones = new Category();
+//        $phones->setName('Phones');
+//
+//        $smartPh = new Category();
+//        $smartPh->setName('Smart Phones');
+//        $smartPh->setParent($phones);
+//
+//        $ph = new Category();
+//        $ph->setName('Dial Phone');
+//        $ph->setParent($phones);
+//
+////        $carrots = new Category();
+////        $carrots->setName('Carrots');
+////        $carrots->setParent($vegetables);
+//
+//        $em->persist($phones);
+//        $em->persist($smartPh);
+//        $em->persist($ph);
+////        $em->persist($carrots);
+//        $em->flush();
+//        die;
+//        $a = new Category();
+//        $a->setName('Expensive');
+//        $b = new Category();
+//        $b->setName('Cheap');
+//        $smart = $repo->findOneByName('Smart Phones');
+//        $repo->persistAsFirstChild($smart)
+//            ->persistAsFirstChildOf($a, $smart)
+//            ->persistAsLastChildOf($b, $smart);
+//
+//        $em->flush();
+//
+//
+        $rendered = [];
+//        $htmlTree = $repo->childrenHierarchy(
+//            null, /* starting from root nodes */
+//            false, /* false: load all children, true: only direct */
+//            array(
+//                'decorate' => true,
+//                'representationField' => 'slug',
+//                'html' => true
+//            )
+//        );
+//        print_r($htmlTree);die;
 
-//        $menu->addChild('Home', array('route' => 'lokos_shop_homepage'));
-//
-//        // access services from the container!
-//        /** @var EntityManager $em */
-//        $em = $this->container->get('doctrine')->getManager();
-//        // findMostRecent and Blog are just imaginary examples
-//        $categories = $em->getRepository('LokosShopBundle:Category')->findAll();
-//
-//        /** @var Category $category */
-//        foreach ($categories as $category) {
-//            $menu->addChild($category->getName(), array(
-//                'route' => 'lokos_shop_overview',
-//                'routeParameters' => array('id' => $category->getId())
-//            ));
-//        }
-//
-//        // create another menu item
-//        $menu->addChild('About Me', array('route' => 'lokos_shop_homepage'));
-//        // you can also add sub level's to your menu's as follows
-//        $menu['About Me']->addChild('Edit profile', array('route' => 'lokos_shop_homepage'));
-//
-//        // ... add more children
+        $options = array(
+            'decorate'      => true,
+            'rootOpen'      => function ($tree) {
+            },
+            'rootClose'     => '</ul></li>'."\n",
+            'childOpen'     => function ($tree) {
+                if (!empty($tree['__children'])) {
+                    $res = '<li><a href="#" class="dropdown-toggle" data-toggle="dropdown">'.$tree["name"].'<b class="caret"></b></a><ul class="dropdown-menu">'."\n";
+                } else {
+                    $res = '<li>'."\n";
+                }
+                return $res;
+            },
+            'childClose'    => '</li>'."\n",
+            'nodeDecorator' => function ($node) {
+                if (!empty($node['__children'])) {
+                    $res = ''."\n";
+                } else {
+                    $res = '<a href="/page/">'.$node['name'].'</a>'."\n";
+                }
+                return $res;
+            }
+        );
 
-        return $menu;
+        $tree = $repo->childrenHierarchy(null,false, $options);
+
+        echo $tree;//die;
+
+//        return $menu;
     }
 }
